074e3093a3abec72d0bf63ded3f4b5be27744753
progress <leswifil@gmail.com>
2023-08-08 14:25:09 +0200

    routes done

--- factory-function/greetFunction.js
+++ factory-function/greetFunction.js
@@ -1,80 +1,87 @@
 export default function greet() {
-    var counter = 0;
-    var userLanguage = '';
-    var userNames = [];
-    var userName = '';
-    var greet = '';
-    var error = '';  
-  
-    function setName(name) {
-      userName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-      return userName;
-    }
-  
-    function setLanguage(language) {
-      userLanguage = language;
-      return userLanguage;
-    }
-  
-    function getCounter() {
-      return counter;
-    }
-  
-    function getNames() {
-      return userNames;
-    }
-  
-    function addName(name) {
-      var userName = setName(name);
-      if (!userNames.includes(userName)) {
-        userNames.push(userName);
-        counter = userNames.length;
-      }
-      return counter
-    }
-  
-    function greetUser() {
-      userName = setName(userName);
-  
-      if (userLanguage !== '' && userName.match(/^[A-Za-z]+$/) && !getNames().includes(userName)) {
-        addName(userName);
-        if (userLanguage === 'english') {
-          greet = 'Hi ' + userName;
-        } else if (userLanguage === 'afrikaans') {
-          greet = 'Hallo ' + userName;
-        } else if (userLanguage === 'xhosa') {
-          greet = 'Molo ' + userName;
-        } 
-      }
-      return greet;
-    }
+  var counter = 0;
+  var userLanguage = "";
+  var userNames = []; 
+  var userName = "";
+  var greet = "";
+  var error = "";
 
-    function errorMessage() {
-     if (!userName.match(/^[A-Za-z]+$/) && userLanguage === '') {
-        error = 'Enter a valid name and select a language';
-      } else if (userLanguage === '') {
-        error = 'Select a language';
-      } else if (!userName.match(/^[A-Za-z]+$/)) {
-        error = 'Enter a valid name';
-      }
-      return error;
-    }
+  function setName(name) {
+    userName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    return userName;
+  }
+
+  function setLanguage(language) {
+    userLanguage = language;
+    return userLanguage;
+  }
+
+  function getCounter() {
+    return counter;
+  }
+
+  function getNames() {
+    return userNames.map((user) => user.name); 
+  }
+
+  //
+  function addName(name) {
+    const userName = setName(name);
+    const user = userNames.find(user => user.name === userName);
   
-    function resetCounter() {
-      counter = 0;
-      return counter;
+    if (!user) {
+      userNames.push({ name: userName, greetCount: 1 });
+    } else {
+      user.greetCount++;
     }
-  
-    return {
-      greetUser,
-      getCounter,
-      getNames,
-      setName,
-      setLanguage,
-      resetCounter,
-      errorMessage,
-      addName,
-    };
+    counter++;
+    return counter;
   }
   
 
+  function greetUser() {
+    if (userLanguage !== "" && userName.match(/^[A-Za-z]+$/)) {
+      addName(userName);
+      if (userLanguage === "english") {
+        greet = "Hi " + userName;
+      } else if (userLanguage === "afrikaans") {
+        greet = "Hallo " + userName;
+      } else if (userLanguage === "xhosa") {
+        greet = "Molo " + userName;
+      }
+    }
+    return greet;
+  }
+
+  function errorMessage() {
+    if (!userName.match(/^[A-Za-z]+$/) && userLanguage === "") {
+      error = "Enter a valid name and select a language";
+    } else if (userLanguage === "") {
+      error = "Select a language";
+    } else if (!userName.match(/^[A-Za-z]+$/)) {
+      error = "Enter a valid name";
+    }
+    return error;
+  }
+
+  function reset() {
+    counter = 0;
+    userNames = [];
+  }
+
+  function getUserGreetCount(userName) {
+    return (userNames.find(user => user.name === userName) || {}).greetCount || 0;
+  }
+  
+  return {
+    greetUser,
+    getCounter,
+    getNames,
+    setName,
+    setLanguage,
+    reset,
+    errorMessage,
+    addName,
+    getUserGreetCount,
+  };
+}
--- index.js
+++ index.js
@@ -2,9 +2,19 @@ import express from "express";
 import { engine } from "express-handlebars";
 import bodyParser from "body-parser";
 import greet from "./factory-function/greetFunction.js";
+import flash from "connect-flash";
+import session from "express-session";
 
 const app = express();
 const greetFunction = greet();
+
+app.use(session({ 
+  secret: 'loot',
+  resave: false, 
+  saveUninitialized: false
+}));
+
+app.use(flash());
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
@@ -26,24 +36,32 @@ app.post('/', function (req, res) {
 
   greetFunction.setName(name);
   greetFunction.setLanguage(language);
-  res.render('index', {
-    greeting: greetFunction.greetUser(),
-    counter: greetFunction.getCounter(),
-  });
+  greetFunction.reset();
+  // const errorMessage = greetFunction.errorMessage(name, language);
+  // if (errorMessage) {
+  //   req.flash("error", errorMessage);
+  // }
+  res.redirect('/');
 });
 
-// app.post("/counter", function (req, res) {
-//   res.redirect("/");
-// });
+app.get('/greeted', function (req, res) {
+  const greetedNames = greetFunction.getNames().map(name => ({
+    name,
+    greetCount: greetFunction.getUserGreetCount(name),
+  }));
+  res.render("greeted", { greetedNames });
+});
 
-
-// app.post("/userGreeted", function (req, res) {
-//   res.render("index", {
-//   });
-// });
-
+app.get('/counter/:name', function (req, res) {
+  const name = req.params.name;
+  const greetCount = greetFunction.getUserGreetCount(name);
+  const counter = greetFunction.getCounter();
+  res.render('counter', { name, greetCount, counter });
+});
 const PORT = process.env.PORT || 3007;
 app.listen(PORT, function () {
   console.log("App started at port:", PORT);
 });
+
+
 
